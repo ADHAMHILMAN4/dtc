fa007deb5dd4d3bfbd6cdea77338acdc29775651
ADHAMHILMAN4 <adhamhilman26@gmail.com>
2026-08-25 12:34:10 +0800

    Feat: Login Page UI

--- src/frontend/dtc-web/src/app/app.routes.ts
+++ src/frontend/dtc-web/src/app/app.routes.ts
@@ -11,8 +11,13 @@ export const routes: Routes = [
                 path: '',
                 loadComponent: () => import('./features/landing/landing').then((m) => m.Landing),
             },
+            {
+                path: 'login',
+                loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
+            },
         ],
     },
+
     {
         path: 'dashboard',
         component: Dashboard,
