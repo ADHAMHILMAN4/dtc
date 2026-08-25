9b856af99aeda97d9772f37130fe2ecf53edcddf
ADHAMHILMAN4 <adhamhilman26@gmail.com>
2026-08-25 12:45:04 +0800

    Feat: Register Page UI

--- src/frontend/dtc-web/src/app/app.routes.ts
+++ src/frontend/dtc-web/src/app/app.routes.ts
@@ -15,6 +15,10 @@ export const routes: Routes = [
                 path: 'login',
                 loadComponent: () => import('./features/auth/login/login').then((m) => m.Login),
             },
+            {
+                path: 'register',
+                loadComponent: () => import('./features/auth/register/register').then((m) => m.Register),
+            },
         ],
     },
 
