38ac52a7a54da76ce2fbfc9060ffb2275f8b1c92
ADHAMHILMAN4 <adhamhilman26@gmail.com>
2026-08-25 12:10:29 +0800

    Feat: Landing Page UI

--- src/frontend/dtc-web/src/app/app.routes.ts
+++ src/frontend/dtc-web/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
+import { PublicLayout } from './layout/public-layout/public-layout';
 import { Dashboard } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: Dashboard
-  }
+    {
+        path: '',
+        component: PublicLayout,
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./features/landing/landing').then((m) => m.Landing),
+            },
+        ],
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard,
+    },
 ];
